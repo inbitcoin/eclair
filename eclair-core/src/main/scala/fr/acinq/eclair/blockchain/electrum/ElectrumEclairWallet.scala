f4ba6d75b4cb2c7b4a3733b4d7444ff9fc100b23
pm47 <pm.padiou@gmail.com>
2019-05-14 18:05:40 +0200

    use bitcoinj to publish txes in p2p

--- eclair-core/src/main/scala/fr/acinq/eclair/blockchain/electrum/ElectrumEclairWallet.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/blockchain/electrum/ElectrumEclairWallet.scala
@@ -18,17 +18,28 @@ package fr.acinq.eclair.blockchain.electrum
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.pattern.ask
+import com.google.common.util.concurrent.{FutureCallback, Futures, MoreExecutors}
 import fr.acinq.bitcoin.{ByteVector32, Satoshi, Script, Transaction, TxOut}
 import fr.acinq.eclair.addressToPublicKeyScript
 import fr.acinq.eclair.blockchain.electrum.ElectrumClient.BroadcastTransaction
 import fr.acinq.eclair.blockchain.electrum.ElectrumWallet._
 import fr.acinq.eclair.blockchain.{EclairWallet, MakeFundingTxResponse}
 import grizzled.slf4j.Logging
+import org.bitcoinj.core.PeerGroup
+import org.bitcoinj.net.discovery.DnsDiscovery
+import org.bitcoinj.params.TestNet3Params
 import scodec.bits.ByteVector
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 class ElectrumEclairWallet(val wallet: ActorRef, chainHash: ByteVector32)(implicit system: ActorSystem, ec: ExecutionContext, timeout: akka.util.Timeout) extends EclairWallet with Logging {
+
+  val params = TestNet3Params.get()
+  val peerGroup = new PeerGroup(params, null)
+  peerGroup.setMaxConnections(3)
+  peerGroup.addPeerDiscovery(new DnsDiscovery(params))
+  peerGroup.start()
+  logger.info(s"bitcoinj peergroup started")
 
   override def getBalance = (wallet ? GetBalance).mapTo[GetBalanceResponse].map(balance => balance.confirmed + balance.unconfirmed)
 
@@ -44,26 +55,31 @@ class ElectrumEclairWallet(val wallet: ActorRef, chainHash: ByteVector32)(implic
     })
   }
 
-  override def commit(tx: Transaction): Future[Boolean] =
-    (wallet ? BroadcastTransaction(tx)) flatMap {
-      case ElectrumClient.BroadcastTransactionResponse(tx, None) =>
+  override def commit(tx: Transaction): Future[Boolean] = {
+    // this is to disambiguate bitcoin-lib's Transaction and bitcoinj's Transaction
+    import org.bitcoinj.core.{Transaction => Transactionj}
+    val broadcast = peerGroup.broadcastTransaction(new Transactionj(params, tx.bin.toArray))
+    val res = Promise[Boolean]()
+    Futures.addCallback(broadcast.future, new FutureCallback[Transactionj]() {
+      override def onSuccess(transaction: Transactionj): Unit = {
         //tx broadcast successfully: commit tx
-        wallet ? CommitTransaction(tx)
-      case ElectrumClient.BroadcastTransactionResponse(tx, Some(error)) if error.message.contains("transaction already in block chain") =>
-        // tx was already in the blockchain, that's weird but it is OK
-        wallet ? CommitTransaction(tx)
-      case ElectrumClient.BroadcastTransactionResponse(_, Some(error)) =>
+        // TODO: what if this ask fails?
+        (wallet ? CommitTransaction(tx)) map {
+          case CommitTransactionResponse(_) => res.success(true)
+        }
+      }
+      def onFailure(throwable: Throwable): Unit = { // This can happen if we get a reject message from a peer.
+        // TODO: make sure that the tx will never get published!
         //tx broadcast failed: cancel tx
-        logger.error(s"cannot broadcast tx ${tx.txid}: $error")
-        wallet ? CancelTransaction(tx)
-      case ElectrumClient.ServerError(ElectrumClient.BroadcastTransaction(tx), error) =>
-        //tx broadcast failed: cancel tx
-        logger.error(s"cannot broadcast tx ${tx.txid}: $error")
-        wallet ? CancelTransaction(tx)
-    } map {
-      case CommitTransactionResponse(_) => true
-      case CancelTransactionResponse(_) => false
-    }
+        logger.error(s"cannot broadcast tx ${tx.txid}: ", throwable)
+        // TODO: what if this ask fails?
+        (wallet ? CancelTransaction(tx)) map {
+          case CancelTransactionResponse(_) => res.success(false)
+        }
+      }
+    }, MoreExecutors.directExecutor)
+    res.future
+  }
 
   def sendPayment(amount: Satoshi, address: String, feeRatePerKw: Long): Future[String] = {
     val publicKeyScript = Script.write(addressToPublicKeyScript(address, chainHash))
